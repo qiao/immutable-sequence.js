ce1519b73d50bb709ab20d45689ee33cd9c94ff2
Xueqiao Xu <xueqiaoxu@gmail.com>
2014-07-15 10:52:48 -0700

    mod: test api

--- test/immutable_sequence.test.js
+++ test/immutable_sequence.test.js
@@ -5,30 +5,30 @@ var ImmutableSequence = require('..');
 describe('Immutable Sequence', function () {
   it('should construct an empty immutable sequence when given an empty array', function () {
     var s = ImmutableSequence.fromArray([]);
-    s.length.should.eql(0);
+    s.size().should.eql(0);
   });
 
   it('should construct an immutable sequence containing initial elements', function () {
     var s = ImmutableSequence.fromArray([1, 2, 3]);
-    s.length.should.eql(3);
+    s.size().should.eql(3);
   });
 
-  it('should be able to push an element to the end', function () {
+  it('should be able to add an element to the end', function () {
     var s = ImmutableSequence.fromArray([]);
-    s.push(1);
-    s.length.should.eql(1);
+    s = s.addLast(1);
+    s.size().should.eql(1);
   });
 
-  it('should be able to unshift an element to the front', function () {
+  it('should be able to add an element to the front', function () {
     var s = ImmutableSequence.fromArray([]);
-    s.unshift(1);
-    s.length.should.eql(1);
+    s = s.addFirst(1);
+    s.size().should.eql(1);
   });
 
   it('should be able get the element at a certain position', function () {
     var s = ImmutableSequence.fromArray([1, 2, 3]);
-    s.get(0).should.be(1);
-    s.get(1).should.be(2);
+    s.get(0).should.eql(1);
+    s.get(1).should.eql(2);
     (function () {
       s.get(3);
     }).should.throw();
@@ -37,7 +37,7 @@ describe('Immutable Sequence', function () {
   it('should be able to split at a certain position', function () {
     var s = ImmutableSequence.fromArray([1, 2, 3]);
     var split = s.splitAt(1);
-    split[0].length.should.eql(2);
-    split[1].length.should.eql(1);
+    split[0].size().should.eql(2);
+    split[1].size().should.eql(1);
   });
 });
